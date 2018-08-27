dfeba7785b81ee8d96088df3a13612e3e27dd26d
John Cody Thompson <johnthompson@Johns-MacBook-Pro.local>
2018-08-27 10:57:38 -0600

    drawer tableview an cells

---
+++ wrk.out/views/LiftHeaderTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  LiftHeaderTableViewCell.swift
+//  TestTimer
+//
+//  Created by Eric Lanza on 8/24/18.
+//  Copyright © 2018 ETLanza. All rights reserved.
+//
+
+import UIKit
+
+class LiftHeaderTableViewCell: UITableViewCell {
+    
+}
