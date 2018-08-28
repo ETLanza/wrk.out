282dcac9642b5598f2c105ffc91c2f3f604cecca
Sam <SamsCodingGrind@MacBook-Pro.local>
2018-08-28 16:33:16 -0600

    API call finished? with category

--- wrk.out/Controllers/Model Controllers/SearchController.swift
+++ wrk.out/Controllers/Model Controllers/SearchController.swift
@@ -20,42 +20,42 @@ class SearchController {
     //  static let baseURL = URL(string: "https://wger.de/api/v2/exerciseinfo/?format=json&language=2&status=2")
     
     static func getWorkouts (completion: @escaping ([Exercise]?) -> Void) {
-        guard let aurl = baseURL else {completion(nil) ; return}
-        var components = URLComponents(url: aurl, resolvingAgainstBaseURL: true)
-        let languageQuery = URLQueryItem(name: "language", value: "2")
-        // TODO: Make this a variable which changes after each iteration until 10
-        let pageQuery = URLQueryItem(name: "page", value: "1")
-        let statusQuery = URLQueryItem(name: "status", value: "2")
-        components?.queryItems = [languageQuery, pageQuery, statusQuery]
-        guard let dataTaskURL = components?.url else { completion(nil) ; return }
-
-        var request = URLRequest(url: dataTaskURL)
-        
-        request.httpBody = nil
-        request.httpMethod = "GET"
-        
-        let dataTask = URLSession.shared.dataTask(with: request) { (data, _, error) in
-            if let error = error {
-                print("there was an error handling the dataTask \(error.localizedDescription)")
-                completion(nil) ; return
+        var pageNumber: Int = 1
+        while pageNumber < 11 {
+            guard let aurl = baseURL else {completion(nil) ; return}
+            var components = URLComponents(url: aurl, resolvingAgainstBaseURL: true)
+            let languageQuery = URLQueryItem(name: "language", value: "2")
+            let pageQuery = URLQueryItem(name: "page", value: String(pageNumber))
+            let statusQuery = URLQueryItem(name: "status", value: "2")
+            components?.queryItems = [languageQuery, pageQuery, statusQuery]
+            guard let dataTaskURL = components?.url else { completion(nil) ; return }
+            
+            var request = URLRequest(url: dataTaskURL)
+            
+            request.httpBody = nil
+            request.httpMethod = "GET"
+            
+            let dataTask = URLSession.shared.dataTask(with: request) { (data, _, error) in
+                if let error = error {
+                    print("there was an error handling the dataTask \(error.localizedDescription)")
+                    completion(nil) ; return
+                }
+                guard let data = data else { completion (nil) ; return }
+                let jsonDecoder = JSONDecoder()
+                do {
+                    let exercises = try jsonDecoder.decode(TopLevelDictionary.self, from: data)
+                    SearchController.shared.excercises += exercises.results
+                    completion(exercises.results)
+                } catch {
+                    print("there was an error decoding workouts from json")
+                }
             }
-            guard let data = data else { completion (nil) ; return }
-            let jsonDecoder = JSONDecoder()
-            do {
-                let exercises = try jsonDecoder.decode(TopLevelDictionary.self, from: data)
-                SearchController.shared.excercises = exercises.results
-                completion(exercises.results)
-            } catch {
-                print("there was an error decoding workouts from json")
-            }
+            pageNumber = pageNumber + 1
+            dataTask.resume()
         }
-        dataTask.resume()
-    }
-    static func getWorkoutImagesMaybe() {
-        
     }
     
-    static func getWorkoutDescription() {
+    static func getWorkoutImagesMaybe() {
         
     }
 }
