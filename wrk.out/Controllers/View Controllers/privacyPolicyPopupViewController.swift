676b701b71487bbc0efa48fba8d096f0c9d2298e
Eric Lanza <eric@lanza.io>
2018-08-28 11:09:31 -0600

    Fix merge conflict

---
+++ wrk.out/Controllers/View Controllers/privacyPolicyPopupViewController.swift
@@ -0,0 +1,22 @@
+//
+//  privacyPolicyPopupViewController.swift
+//  wrk.out
+//
+//  Created by John Cody Thompson on 8/27/18.
+//  Copyright © 2018 ETLanza. All rights reserved.
+//
+
+import UIKit
+
+class privacyPolicyPopupViewController: UIViewController {
+    
+    
+    @IBOutlet weak var exitButton: UIButton!
+    @IBOutlet weak var popupView: UIView!
+    
+    @IBAction func exitButtonTapped(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+  
+}
